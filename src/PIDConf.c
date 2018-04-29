db0c28ec729fe39874bffdf7c7a53d04cee62c29
alexander <lonsas@gmail.com>
2018-04-29 11:41:14 +0200

    Tuning for first flight test

--- src/PIDConf.c
+++ src/PIDConf.c
@@ -5,7 +5,7 @@
 #include "eeprom.h"
 
 static const PidParameters gyroDefaultPidParameters = {
-  .K = 0.05,
+  .K = 0.15,
   .Ti = 0,
   .Td = 0,
   .Tt = 0,
@@ -14,7 +14,7 @@ static const PidParameters gyroDefaultPidParameters = {
 };
 
 static const PidParameters angleDefaultPidParameters = {
-  .K = 100,
+  .K = 125,
   .Ti = 0,
   .Td = 0,
   .Tt = 0,
